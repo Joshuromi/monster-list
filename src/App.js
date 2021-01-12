5a50745a21edd8455fd0284d245a3fb738a5a400
Josh Ojuromi <45287264+Joshuromi@users.noreply.github.com>
2021-01-12 12:59:20 +0100

    initialize react and react-dom environment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+// import {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   const [change, setChange] = useState(true);
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         {
+//           change === true ? <p>Hello Josh!</p> : <p>Changed</p>
+//         }
+//         <button onClick = {(()=>(setChange(!change)))}>Change Text</button>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}));
+  }
+
+  render() {
+    return (
+      <div className="App">
+       {
+         this.state.monsters.map((monster) => <h3 key={monster.id}>{monster.name}</h3>)
+       }
+      </div>
+    );
+  }
 }
 
 export default App;
