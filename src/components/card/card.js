c3ca074019c8e47299447ea6616933a436332c42
Josh Ojuromi <45287264+Joshuromi@users.noreply.github.com>
2021-01-14 01:05:12 +0100

    added styling to background and heading

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -4,7 +4,7 @@ import './card.css';
 const Card = props => {
     return(
         <div className="card-container">
-            <img src={`https://robohash.org/${props.monster.id}?set=set5&size=180x180`} alt="#" />
+            <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="#" />
             <h3>{props.monster.name}</h3>
             <p>{props.monster.email}</p>
         </div>
